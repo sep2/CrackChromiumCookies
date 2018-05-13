562683d8f99c1a648c9ad860cba24ed215b9eded
sep2 <39055101+sep2@users.noreply.github.com>
2018-05-13 23:28:30 +0800

    Update CrackChromiumCookies.h
    fix lic

--- include/CrackChromiumCookies.h
+++ include/CrackChromiumCookies.h
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2018 Felix (i at sep2 dot org)
+// Copyright (c) 2018 LCZ (i at lcz dot link)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
